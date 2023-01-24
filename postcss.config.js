53e671c21163d31841f7f4dedde70bc072432857
brunoorosco <brunoorosco@gmail.com>
2023-01-23 23:04:47 -0300

    chore: ajsute postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    plugins: [
-    'postcss-import',
-     'autoprefixer',
-     'tailwindcss'
-    ]
-  }
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    }
+}
