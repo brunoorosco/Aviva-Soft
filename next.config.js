6deac810a636cebd3cfbcb4d3c874f0fd92fbdbf
brunoorosco <brunoorosco@gmail.com>
2023-01-30 15:00:59 -0300

    fix: ajuste next.conf

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => ({
+    reactStrictMode: true,
+    pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts']
+        // pageExtensions: ['ts', 'tsx', 'js', 'jsx']
+        //     .map((extension) => {
+        //         const isDevServer = phase === PHASE_DEVELOPMENT_SERVER
+        //         const prefixes = isDevServer ? ['dev', 'prod'] : ['prod']
+        //         return prefixes.map((prefix) => `${prefix}.${extension}`)
+        //     })
+        //     .flat()
+})
